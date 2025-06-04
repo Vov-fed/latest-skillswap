532d57f136a88412f7816e73eb5a11ff6dc3ebc3
Владимир Федорук <vladimirfedoruk@Vladimirs-MacBook-Pro.local>
2025-06-04 17:12:27 +0300

    added real backend--fixed

--- frontend/src/services/chatAndMessageApi.ts
+++ frontend/src/services/chatAndMessageApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 import Cookies from "js-cookie";
 
 const token = Cookies.get("token");
+const BACKEND_URI = import.meta.env.BACKEND_URI || "https://latest-skillswap-production.up.railway.app";
+console.log("Using backend URI:", BACKEND_URI);
 const api = axios.create({
-  baseURL: process.env.BACKEND_URI || "http://localhost:3000", // add http! and adjust if needed
+  baseURL: BACKEND_URI || "http://localhost:3000", // add http! and adjust if needed
   headers: { authorization: token }
 });
 
